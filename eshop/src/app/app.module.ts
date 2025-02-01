9b0dc9a5fccbac49a3b0187c09d221ef2b959bbf
Mihajlo <nmihajlon@gmail.com>
2025-02-01 16:39:55 +0100

    header component

--- eshop/src/app/app.module.ts
+++ eshop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
