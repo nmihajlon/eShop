6e9375c310bfb92a98d9fceb196af1a0903eb7ff
Mihajlo <nmihajlon@gmail.com>
2025-02-04 14:56:09 +0100

    init of custom directive

--- eshop/src/app/app.module.ts
+++ eshop/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ProductComponent } from './container/product-list/product/product.compo
 import { FilterComponent } from './container/product-list/filter/filter.component';
 import { ProductDetailComponent } from './container/product-detail/product-detail.component';
 import { FeaturedBrandsComponent } from './container/featured-brands/featured-brands.component';
+import { SetBackground } from './CustomDirectives/SetBackground.directive';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { FeaturedBrandsComponent } from './container/featured-brands/featured-br
     FilterComponent,
     ProductDetailComponent,
     FeaturedBrandsComponent,
+    SetBackground
   ],
   imports: [
     BrowserModule,
